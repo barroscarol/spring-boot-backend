14320ec83272bf04d37ec24f9f170433b0e0c83f
Carol Barros <carolbarrosnasc@gmail.com>
2020-12-28 16:30:32 -0300

    Primeira versão do REST-API com SpringBoot

---
+++ src/main/java/com/nelioalves/cursomc/resource/CategoriaResource.java
@@ -0,0 +1,27 @@
+package com.nelioalves.cursomc.resource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.cursomc.domain.Categoria;
+
+@RestController
+@RequestMapping(value="/categorias")
+public class CategoriaResource {
+
+
+		public List<Categoria> listar() {
+
+			Categoria cat1 = new Categoria(1, "Informática");
+			Categoria cat2 = new Categoria(2, "Escritório");
+
+			List<Categoria> lista = new ArrayList<>();
+			lista.add(cat1);
+			lista.add(cat2);
+
+			return lista;
+	}
+}
