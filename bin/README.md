8b917407f8f1c3b5ca4f695463c20762818d07e8
Carol Barros <carolbarrosnasc@gmail.com>
2021-01-02 20:36:42 -0300

    Inserindo nova Categoria com POST

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# SpringBootApp
+Projeto Java Spring Boot no back end e Ionic no front end. Do design UML ao Heroku.
--- src/main/java/com/nelioalves/cursomc/resource/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resource/CategoriaResource.java
@@ -1,13 +1,16 @@
 package com.nelioalves.cursomc.resource;
 
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.services.CategoriaService;
@@ -24,6 +27,21 @@ public class CategoriaResource {
 
 		Optional<Categoria> obj = Optional.ofNullable(service.find(id));
 		return ResponseEntity.ok().body(obj);
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		/* Inserir objeto e retornar a sua URI nova */
+
+		obj = service.insert(obj);
+
+		/*
+		 * Retornando a URI do .fromCurrentRequest() - pega a URL nova, atribui o valor
+		 * com .buildAndExpand"
+		 */
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
 	}
 
 }
